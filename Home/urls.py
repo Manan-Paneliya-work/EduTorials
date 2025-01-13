ed66e11ccf2202f2252f549238184821d024f5db
Manan Paneliya <mananpaneliya0@gmail.com>
2025-01-14 00:15:28 +0530

    code commited

--- Home/urls.py
+++ Home/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('courses/', views.courses, name='courses'),  # About Us page
     # path('contact/', views.contact, name='contact'),  # Contact Us page
     # path('register/', views.register, name='register'),  # Register page
-    # path('login/', views.login_view, name='login'),  # Login page
+    # path('login/', views.login_view, name='login'),  # Login page .
 ]
